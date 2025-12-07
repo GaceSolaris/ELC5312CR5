19326b7d65aeceae93793cb39ad187270ccdf73c
Julieus Markle <77992455+GaceSolaris@users.noreply.github.com>
2025-12-07 17:01:40 -0600

    Merged Master File

---
+++ projectfiles/vitisfiles/main_sampler_test.cpp
@@ -0,0 +1,359 @@
+/*****************************************************************//**
+ * @file main_sampler_test.cpp
+ *
+ * @brief Basic test of nexys4 ddr mmio cores
+ *
+ * @author p chu
+ * @version v1.0: initial release
+ *********************************************************************/
+
+// #define _DEBUG
+#include "chu_init.h"
+#include "gpio_cores.h"
+//#include "xadc_core.h"
+#include "sseg_core.h"
+//#include "spi_core.h"
+#include "i2c_core.h"
+//#include "ps2_core.h"
+//#include "ddfs_core.h"
+//#include "adsr_core.h"
+
+/**
+ * blink once per second for 5 times.
+ * provide a sanity check for timer (based on SYS_CLK_FREQ)
+ * @param led_p pointer to led instance
+ */
+void timer_check(GpoCore *led_p) {
+   int i;
+
+   for (i = 0; i < 5; i++) {
+      led_p->write(0xffff);
+      sleep_ms(500);
+      led_p->write(0x0000);
+      sleep_ms(500);
+      debug("timer check - (loop #)/now: ", i, now_ms());
+   }
+}
+
+/**
+ * check individual led
+ * @param led_p pointer to led instance
+ * @param n number of led
+ */
+void led_check(GpoCore *led_p, int n) {
+   int i;
+
+   for (i = 0; i < n; i++) {
+      led_p->write(1, i);
+      sleep_ms(100);
+      led_p->write(0, i);
+      sleep_ms(100);
+   }
+}
+
+/**
+ * leds flash according to switch positions.
+ * @param led_p pointer to led instance
+ * @param sw_p pointer to switch instance
+ */
+void sw_check(GpoCore *led_p, GpiCore *sw_p) {
+   int i, s;
+
+   s = sw_p->read();
+   for (i = 0; i < 30; i++) {
+      led_p->write(s);
+      sleep_ms(50);
+      led_p->write(0);
+      sleep_ms(50);
+   }
+}
+
+/**
+ * uart transmits test line.
+ * @note uart instance is declared as global variable in chu_io_basic.h
+ */
+void uart_check() {
+   static int loop = 0;
+
+   uart.disp("uart test #");
+   uart.disp(loop);
+   uart.disp("\n\r");
+   loop++;
+}
+
+/**
+ * read FPGA internal voltage temperature
+ * @param adc_p pointer to xadc instance
+ */
+
+/**
+ * tri-color led dims gradually
+ * @param led_p pointer to led instance
+ * @param sw_p pointer to switch instance
+ */
+
+void pwm_3color_led_check(PwmCore *pwm_p) {
+   int i, n;
+   double bright, duty;
+   const double P20 = 1.2589;  // P20=100^(1/20); i.e., P20^20=100
+
+   pwm_p->set_freq(50);
+   for (n = 0; n < 3; n++) {
+      bright = 1.0;
+      for (i = 0; i < 20; i++) {
+         bright = bright * P20;
+         duty = bright / 100.0;
+         pwm_p->set_duty(duty, n);
+         pwm_p->set_duty(duty, n + 3);
+         sleep_ms(100);
+      }
+      sleep_ms(300);
+      pwm_p->set_duty(0.0, n);
+      pwm_p->set_duty(0.0, n + 3);
+   }
+}
+
+/**
+ * Test debounced buttons
+ *   - count transitions of normal and debounced button
+ * @param db_p pointer to debouceCore instance
+ */
+
+void debounce_check(DebounceCore *db_p, GpoCore *led_p) {
+   long start_time;
+   int btn_old, db_old, btn_new, db_new;
+   int b = 0;
+   int d = 0;
+   uint32_t ptn;
+
+   start_time = now_ms();
+   btn_old = db_p->read();
+   db_old = db_p->read_db();
+   do {
+      btn_new = db_p->read();
+      db_new = db_p->read_db();
+      if (btn_old != btn_new) {
+         b = b + 1;
+         btn_old = btn_new;
+      }
+      if (db_old != db_new) {
+         d = d + 1;
+         db_old = db_new;
+      }
+      ptn = d & 0x0000000f;
+      ptn = ptn | (b & 0x0000000f) << 4;
+      led_p->write(ptn);
+   } while ((now_ms() - start_time) < 5000);
+}
+
+/**
+ * Test pattern in 7-segment LEDs
+ * @param sseg_p pointer to 7-seg LED instance
+ */
+
+void sseg_check(SsegCore *sseg_p) {
+   int i, n;
+   uint8_t dp;
+
+   //turn off led
+   for (i = 0; i < 8; i++) {
+      sseg_p->write_1ptn(0xff, i);
+   }
+   //turn off all decimal points
+   sseg_p->set_dp(0x00);
+
+   // display 0x0 to 0xf in 4 epochs
+   // upper 4  digits mirror the lower 4
+   for (n = 0; n < 4; n++) {
+      for (i = 0; i < 4; i++) {
+         //sseg_p->write_1ptn(sseg_p->h2s(i + n * 4), 3 - i);
+         sseg_p->write_1ptn(sseg_p->h2s(i + n * 4), 7 - i);
+         sleep_ms(300);
+      } // for i
+   }  // for n
+      // shift a decimal point 4 times
+   for (i = 0; i < 4; i++) {
+      bit_set(dp, 7 - i);
+      sseg_p->set_dp(1 << (7 - i));
+      sleep_ms(300);
+   }
+   //turn off led
+   for (i = 0; i < 8; i++) {
+      sseg_p->write_1ptn(0xff, i);
+   }
+   //turn off all decimal points
+   sseg_p->set_dp(0x00);
+
+}
+
+/*
+ * read temperature from adt7420
+ * @param adt7420_p pointer to adt7420 instance
+ */
+void adt7420_check(I2cCore *adt7420_p, GpoCore *led_p) {
+   const uint8_t DEV_ADDR = 0x4b;
+   uint8_t wbytes[2], bytes[2];
+   //int ack;
+   uint16_t tmp;
+   float tmpC;
+
+   // read adt7420 id register to verify device existence
+   // ack = adt7420_p->read_dev_reg_byte(DEV_ADDR, 0x0b, &id);
+
+   wbytes[0] = 0x0b;
+   adt7420_p->write_transaction(DEV_ADDR, wbytes, 1, 0);
+   adt7420_p->read_transaction(DEV_ADDR, bytes, 1, 0);
+   uart.disp("read ADT7420 id (should be 0xcb): ");
+   uart.disp(bytes[0], 16);
+   uart.disp("\n\r");
+   //debug("ADT check ack/id: ", ack, bytes[0]);
+   // read 2 bytes
+   //ack = adt7420_p->read_dev_reg_bytes(DEV_ADDR, 0x0, bytes, 2);
+   wbytes[0] = 0x00;
+   adt7420_p->write_transaction(DEV_ADDR, wbytes, 1, 0);
+   adt7420_p->read_transaction(DEV_ADDR, bytes, 2, 0);
+   
+   // conversion
+   tmp = (uint16_t) bytes[0];
+   tmp = (tmp << 8) + (uint16_t) bytes[1];
+   if (tmp & 0x8000) {
+      tmp = tmp >> 3;
+      tmpC = (float) ((int) tmp - 8192) / 16;
+   } else {
+      tmp = tmp >> 3;
+      tmpC = (float) tmp / 16;
+   }
+   
+   uart.disp("temperature (C): ");
+   uart.disp(tmpC);
+   uart.disp("\n\r");
+   led_p->write(tmp);
+   sleep_ms(1000);
+   led_p->write(0);
+}
+
+/**
+ * core test
+ * @param led_p pointer to led instance
+ * @param sw_p pointer to switch instance
+ */
+void show_test_id(int n, GpoCore *led_p) {
+   int i, ptn;
+
+   ptn = n; //1 << n;
+   for (i = 0; i < 20; i++) {
+      led_p->write(ptn);
+      sleep_ms(30);
+      led_p->write(0);
+      sleep_ms(30);
+   }
+}
+
+/*
+ * Clear the SSEG Display at startup
+ * to remove unneeded values
+ */
+void sseg_clear(SsegCore *sseg_p) {
+   int i, n;
+   uint8_t dp;
+
+   //turn off led
+   for (i = 0; i < 8; i++) {
+      sseg_p->write_1ptn(0xff, i);
+   }
+   //turn off all decimal points
+   sseg_p->set_dp(0x00);
+}
+
+/*
+ * read temperature from adt7420
+ * and display temperature on SSEG Display
+ */
+void temp_disp(I2cCore *adt7420_p, SsegCore *sseg_p) {
+   const uint8_t DEV_ADDR = 0x4b;
+   uint8_t wbytes[2], bytes[2], dp;
+   //int ack;
+   uint16_t tmp, tmp1, tmp2, tmp3, tmp4, tmpF;
+   float tmpC;
+
+   // read adt7420 id register to verify device existence
+   // ack = adt7420_p->read_dev_reg_byte(DEV_ADDR, 0x0b, &id);
+
+   wbytes[0] = 0x0b;
+   adt7420_p->write_transaction(DEV_ADDR, wbytes, 1, 0);
+   adt7420_p->read_transaction(DEV_ADDR, bytes, 1, 0);
+   uart.disp("read ADT7420 id (should be 0xcb): ");
+   uart.disp(bytes[0], 16);
+   uart.disp("\n\r");
+   //debug("ADT check ack/id: ", ack, bytes[0]);
+   // read 2 bytes
+   //ack = adt7420_p->read_dev_reg_bytes(DEV_ADDR, 0x0, bytes, 2);
+   wbytes[0] = 0x00;
+   adt7420_p->write_transaction(DEV_ADDR, wbytes, 1, 0);
+   adt7420_p->read_transaction(DEV_ADDR, bytes, 2, 0);
+   
+   // conversion
+   tmp = (uint16_t) bytes[0];
+   tmp = (tmp << 8) + (uint16_t) bytes[1];
+   if (tmp & 0x8000) {
+      tmp = tmp >> 3;
+      tmpC = (float) ((int) tmp - 8192) / 16;
+   } else {
+      tmp = tmp >> 3;
+      tmpC = (float) tmp / 16;
+   }
+
+   //Conversion from Celsius to Fahrenheit
+   //Also converts float value to int
+   tmpF = (tmpC * 180) + 3200;
+
+   //Math logic to isolate each value of 
+   //temperature for SSEG placement
+   //Second Decimal Position   
+   tmp1 = tmpF%10;
+   //First Decimal Position
+   tmp2 = (tmpF/10)%10;
+   //Ones Position
+   tmp3 = (tmpF/100)%10;
+   //Tens Position
+   tmp4 = tmpF/1000;   
+   
+   //Sets the position for the decimal
+   sseg_p->set_dp(1 << (6));
+   sseg_p->write_1ptn(sseg_p->h2s(tmp4), 7);
+   sseg_p->write_1ptn(sseg_p->h2s(tmp3), 6);
+   sseg_p->write_1ptn(sseg_p->h2s(tmp2), 5);
+   sseg_p->write_1ptn(sseg_p->h2s(tmp1), 4);
+   //Hardcoded value for F for Fahrenheit
+   sseg_p->write_1ptn(sseg_p->h2s(15), 3);
+
+   //Serial output for verifying proper reading of temperature
+   uart.disp("temperature (F): ");
+   uart.disp(tmpF);
+   uart.disp("\n\r");
+   sleep_ms(100);
+}
+
+
+GpoCore led(get_slot_addr(BRIDGE_BASE, S2_LED));
+GpiCore sw(get_slot_addr(BRIDGE_BASE, S3_SW));
+
+PwmCore pwm(get_slot_addr(BRIDGE_BASE, S6_PWM));
+DebounceCore btn(get_slot_addr(BRIDGE_BASE, S7_BTN));
+SsegCore sseg(get_slot_addr(BRIDGE_BASE, S8_SSEG));
+
+I2cCore adt7420(get_slot_addr(BRIDGE_BASE, S10_I2C));
+
+
+
+int main() {
+   //uint8_t id, ;
+
+   //timer_check(&led);
+   sseg_clear(&sseg);
+   while (1) {
+      temp_disp(&adt7420, &sseg);
+
+   } //while
+} //main
+
